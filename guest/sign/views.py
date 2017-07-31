4633bda7997d0b16ace59e0713d5d5ce94fad4c5
lcnuaa <lcnuaa419@163.com>
2017-07-31 23:23:11 +0800

    登录功能

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello Django!")
+    #return HttpResponse("hello Django!")
+    return render(request,"index.html")
