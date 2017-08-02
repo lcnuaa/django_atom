3eab130eb015a7525cd33565b4dbdb42e18214d0
lcnuaa <lcnuaa419@163.com>
2017-08-02 22:14:10 +0800

    8.2登录post

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -19,5 +19,6 @@ from sign import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/$',views.index)
+    url(r'^index/$',views.index),
+    url(r'^login_action/$',views.login_action)
 ]
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,9 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     #return HttpResponse("hello Django!")
     return render(request,"index.html")
+def login_action(request):
+    if request.method=='POST':
+        username=request.POST.get('username','')
+        password=request.POST.get('password','')
+        if username=='admin' and password=='admin':
+            return HttpResponse('login success!')
+        else:
+            return render(request,'index.html',{'error':'username or password error!'})
