f3e8fed7a67bc42cffdaaae18ae195dc80140885
lcnuaa <lcnuaa419@163.com>
2017-08-06 23:12:42 +0800

    8.6 django模型

--- guest/sign/models.py
+++ guest/sign/models.py
@@ -4,3 +4,28 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name=models.CharField(max_length=100)
+    limit=models.IntegerField()
+    status=models.BooleanField()
+    address=models.CharField(max_length=200)
+    strart_time=models.DateTimeField('event time')
+    create_time=models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.name
+
+#嘉宾表
+class Guest(models.Model):
+    event=models.ForeignKey(Event)#关联发布会id
+    realname=models.CharField(max_length=64)
+    phone=models.CharField(max_length=16)
+    email = models.EmailField()
+    sign=models.BooleanField()
+    create_time=models.DateTimeField(auto_now=True)
+
+class Meta:
+    unique_together=("event","phone")
+
+def _str_(self):
+    return self.realname
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -12,15 +14,17 @@ def login_action(request):
     if request.method=='POST':
         username=request.POST.get('username','')
         password=request.POST.get('password','')
-        if username=='admin' and password=='admin':
+        user=auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)#登录
+            request.session['user']=username
             response = HttpResponseRedirect('/event_manage/')
             #response.set_cookie('user', username, 3600)#添加浏览器cookie
-            request.session['user']=username
             return response
         else:
             return render(request,'index.html',{'error':'username or password error!'})
-
+@login_required
 def event_manage(request):
     #username=request.COOKIES.get('user','')#读取浏览器cookie
     username=request.session.get('user','')
-    return render(request,"event_manage.html")
+    return render(request,"event_manage.html",{"user":username})
